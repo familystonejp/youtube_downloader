d0d98ce2ca5c172f5073892ce86dd292d60756e2
familystonejp <familystonejp@yahoo.co.jp>
2022-09-27 16:57:11 +0900

    3rd commit

--- stream_downloader.py
+++ stream_downloader.py
@@ -3,5 +3,9 @@ import keyboard
 
 
 Ip = input('ダウンロード先のアドレスを入力してEnterを押してください:')
-yt_opts = YoutubeDL({'format': 'mp4'})
+Pass = input('passwordを入力してEnterを押してください。必要なければそのままEnterを押してください:')
+yt_opts = YoutubeDL({
+    'format': 'mp4',
+    'videopassword': Pass
+    })
 yt_opts.download(Ip)
